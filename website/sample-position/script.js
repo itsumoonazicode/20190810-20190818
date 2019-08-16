0d7ac039e78e1c09c28793ad327fce8196e0eb20
itsumoonazicode <dyingkouki@gmail.com>
2019-08-16 18:06:30 +0900

    idの値でソートできるように変更

--- website/sample-position/script.js
+++ website/sample-position/script.js
@@ -1,4 +1,19 @@
-const footer = '{"name": ["Date", "Category", "Contact"]}'
+const footer = `{
+	"list": [
+		{
+			"name": "Date",
+			"id": 2
+		},
+		{
+			"name": "Category",
+			"id": 3
+		},
+		{
+			"name": "Monthly",
+			"id": 1
+		}
+	]
+}`
 const FooterList = document.getElementById("footer").children
 // const FooterList = document.querySelectorAll("#footer")
 console.log(FooterList)
@@ -7,12 +22,21 @@ console.log(FooterList)
 
 try {
 	const json = JSON.parse(footer)
-	console.log(json.name)
-	const SortList = json.name
-	SortList.sort()
+	const SortList = Array.from(json["list"])
+	// 比較関数を利用して"id"の値でソート
+	SortList.sort((a, b) => {
+		if(a.id > b.id) {
+			return 1
+		} else if (b.id > a.id) {
+			return -1
+		} else {
+			return 0
+		}
+	})
+	
 	for(let i = 0; FooterList.length > i; i++) {
-		console.log(`nameの${i}番目は、${SortList[i]}です。`)
-		FooterList[i].textContent = SortList[i]
+		console.log(`nameの${i}番目は、${SortList[i].name}です。`)
+		FooterList[i].textContent = SortList[i].name
 	}
 } catch(err) {
 	console.log(err)
