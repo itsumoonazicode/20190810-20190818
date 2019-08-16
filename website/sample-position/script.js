8e69205a1825b778bd2343e0ca46dab8eefe6d93
itsumoonazicode <dyingkouki@gmail.com>
2019-08-16 22:23:58 +0900

    JSONの定義を外部ファイルに移動

--- website/sample-position/script.js
+++ website/sample-position/script.js
@@ -18,26 +18,28 @@ const FooterList = document.getElementById("footer").children
 // const FooterList = document.querySelectorAll("#footer")
 console.log(FooterList)
 
-
-
-try {
-	const json = JSON.parse(footer)
-	const SortList = Array.from(json["list"])
-	// 比較関数を利用して"id"の値でソート
-	SortList.sort((a, b) => {
-		if(a.id > b.id) {
-			return 1
-		} else if (b.id > a.id) {
-			return -1
-		} else {
-			return 0
+// jsonを読み込む
+const DateUrl = "./navList.json"
+fetch(DateUrl)
+	// ここで内部的にJSON.parse()が行なわれている
+	.then((res) => res.json())
+	.then((json) => {
+		const SortList = Array.from(json["FooterList"])
+		console.log(SortList)
+		// 比較関数を利用して"id"の値でソート
+		SortList.sort((a, b) => {
+			if(a.id > b.id) {
+				return 1
+			} else if (b.id > a.id) {
+				return -1
+			} else {
+				return 0
+			}
+		})
+		
+		for(let i = 0; FooterList.length > i; i++) {
+			console.log(`nameの${i}番目は、${SortList[i].name}です。`)
+			FooterList[i].textContent = SortList[i].name
 		}
 	})
-	
-	for(let i = 0; FooterList.length > i; i++) {
-		console.log(`nameの${i}番目は、${SortList[i].name}です。`)
-		FooterList[i].textContent = SortList[i].name
-	}
-} catch(err) {
-	console.log(err)
-}
+
